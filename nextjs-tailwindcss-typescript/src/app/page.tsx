7f2274fc935604ff4ccf05eb9df4e910bd5db340
Marina  Golao Vale <marinasgv@hotmail.com>
2024-08-02 10:08:24 -0300

    Criando temas dentro do tailwind css com next js

--- nextjs-tailwindcss-typescript/src/app/page.tsx
+++ nextjs-tailwindcss-typescript/src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function PageHome() {
-  return <main>Home</main>;
+  return (
+    <main
+      // Usar a cor criada no arquivo tailwind.config
+      className="bg-background"
+    >
+      Home
+    </main>
+  );
+  // Adicionar atributo
+  document.documentElement.setAttribute("light", "true");
+  // Remover atributo
+  document.documentElement.removeAttribute("light");
 }
--- nextjs-tailwindcss-typescript/tailwind.config.ts
+++ nextjs-tailwindcss-typescript/tailwind.config.ts
@@ -3,8 +3,21 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   // Analisa todo o projeto e olhe para os arquivos que estão nesse caminho, terminando com essas extensoes
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  // Configuração de tema
   theme: {
-    extend: {},
+    // Exterder o tema
+    extend: {
+      // Em cores
+      colors: {
+        // Declarando as cores
+        text: "var(--color-text)",
+        error: "var(--color-error)",
+        paper: "var(--color-paper)",
+        primary: "var(--color-primary)",
+        background: "var(--color-background)",
+        "primary-contrast": "var(--color-primary-contrast)",
+      },
+    },
   },
   plugins: [],
 };
