a6a0df7a6b5f66a65317e1218b00efd8adc6f7f9
MarinaGV93 <marinasgv@hotmail.com>
2024-12-11 12:03:55 -0300

    Criando componente de aula para a playlist na tela de player

--- nextjs-tailwindcss-typescript/src/app/player/[courseId]/[classId]/page.tsx
+++ nextjs-tailwindcss-typescript/src/app/player/[courseId]/[classId]/page.tsx
@@ -1,4 +1,6 @@
-import { PlayerHeader } from "@/components/player";
+"use client";
+
+import { PlayerClass, PlayerHeader } from "@/components/player";
 
 export interface Props {
   params: {
@@ -7,7 +9,8 @@ export interface Props {
   };
 }
 
-export default function PagePlayer({ params: { courseId, classId } }: Props) {
+export default function PagePlayer({ params: { 
+  courseId, classId } }: Props) {
   return (
     <>
       <PlayerHeader
@@ -16,6 +19,13 @@ export default function PagePlayer({ params: { courseId, classId } }: Props) {
         subtitle="🏆 Curso de API Rest, Node e Typescript"
       />
       Player {courseId} {classId}
+      <PlayerClass
+        title="API Rest, Node e Typescript: #00 - Apresentação do curso, tecnologias usadas e muito mais"
+        playing
+        done={false}
+        onPlay={() => console.log("play")}
+        onCheck={() => console.log("check")}
+      />
     </>
   );
 }
---
+++ nextjs-tailwindcss-typescript/src/components/player/PlayerClass.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { MdCheckCircle, MdCircle, MdPlayCircleOutline } from "react-icons/md";
+
+interface IPlayerClassrProps {
+  title: string;
+  playing: boolean;
+  done: boolean;
+  onPlay: () => void;
+  onCheck: () => void;
+}
+
+export const PlayerClass = ({
+  title,
+  playing,
+  done,
+  onPlay,
+  onCheck,
+}: IPlayerClassrProps) => {
+  return (
+    <button className="flex gap-6 p-4 items-center" onClick={onPlay}>
+      {!done && (
+        <div
+          className="group"
+          onClick={(e) => {
+            // Parar o evento de click do botão pai. Se ouver um clique, não precisa transmitir esse evento de clique para o pai
+            e.stopPropagation();
+            onCheck();
+          }}
+        >
+          <MdPlayCircleOutline
+            size={24}
+            className="min-w-6 group-hover:hidden"
+          />
+
+          <MdCircle size={24} className="min-w-6 hidden group-hover:block" />
+        </div>
+      )}
+
+      {done && <MdCheckCircle size={24} className="min-w-6 text-green-400" />}
+
+      <div className="flex flex-col gap-1 items-start">
+        <p
+          data-done={done}
+          className="line-clamp-2 text-start data-[done=true]:text-green-400"
+        >
+          {title}
+        </p>
+
+        {playing && (
+          <span
+            className="px-2 py-1 bg-blue-400 rounded-full
+        // Tamanho da fonte (line-height)
+        leading-4"
+          >
+            Reproduzindo
+          </span>
+        )}
+      </div>
+    </button>
+  );
+};
